8d1e72fa77627fdd9fae85ada70b54377778aa8e
Ben <benjamin.colt.tarr@gmail.com>
2026-04-10 19:09:38 -0700

    les deploy

--- apps/controlmylights/controlmylights_server/src/config.rs
+++ apps/controlmylights/controlmylights_server/src/config.rs
@@ -6,8 +6,6 @@ use serde_inline_default::serde_inline_default;
 #[serde_inline_default]
 #[derive(Debug, Deserialize)]
 pub struct Config {
-    #[serde_inline_default(env!("CARGO_CRATE_NAME").to_string())]
-    pub service_name: String,
     #[serde_inline_default("127.0.0.1:3000".to_string())]
     pub bind_address: String,
     #[serde_inline_default(PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("public"))]
