053e3c6e77d4751670d3428f39754bb8abe638fe
Ben <benjamin.colt.tarr@gmail.com>
2026-05-21 21:29:47 -0700

    add build script to only build when for prod

---
+++ apps/controlmylights/controlmylights_server/build.rs
@@ -0,0 +1,32 @@
+use std::{path::Path, process::Command};
+
+fn main() {
+    if !cfg!(debug_assertions) {
+        return;
+    }
+
+    let client_dir = Path::new(env!("CARGO_MANIFEST_DIR"))
+        .parent()
+        .expect("parent")
+        .join("ControlMyLightsClient");
+
+    dbg!(&client_dir);
+
+    let output = Command::new("pnpm")
+        .arg("build")
+        .current_dir(client_dir)
+        .output()
+        .expect("failed to execute client build command");
+
+    let stdout = String::from_utf8_lossy(&output.stdout);
+    let stderr = String::from_utf8_lossy(&output.stderr);
+
+    for line in stdout.lines() {
+        println!("cargo:warning=[stdout] {}", line);
+    }
+    for line in stderr.lines() {
+        println!("cargo:warning=[stderr] {}", line);
+    }
+
+    assert!(output.status.success(), "client build command exited with an error status");
+}
