f38e48a81f79fad7a8cb26d212ee899ac83390bc
Benjamin Tarr <91578843+btarr101@users.noreply.github.com>
2026-05-23 20:12:56 -0700

    Restructure repository into public and private sections (#42)
    * restructure
    
    * fix

--- libs/clockwork_renderer/src/lib.rs
+++
@@ -1,7 +0,0 @@
-#![feature(associated_type_defaults)]
-
-mod adapters;
-pub mod context;
-pub mod handle;
-pub mod maybe_locked;
-pub mod surface;
--- libs/clockwork_renderer/src/main.rs
+++
@@ -1,6 +0,0 @@
-use clockwork_renderer::context::Context;
-
-fn main() {
-    // let (context, surface) = Context::new_handle_with_surface(todo!()).unwrap();
-    todo!()
-}
