3d73af2965cce2779d6b4dc53c4c7875d55f9741
Ben <benjamin.colt.tarr@gmail.com>
2026-06-21 14:18:38 -0700

    working virtual table

--- public/apps/the_peoples_edh_bracket/the-peoples-edh-bracket-client/src/pages/CommunityPage.tsx
+++ public/apps/the_peoples_edh_bracket/the-peoples-edh-bracket-client/src/pages/CommunityPage.tsx
@@ -1,12 +1,15 @@
 import { Autocomplete, Box, Button, Group, Stack, Table } from '@mantine/core'
 import { MagnifyingGlassIcon } from '@phosphor-icons/react'
+import { useWindowVirtualizer } from '@tanstack/react-virtual'
 import { useQueryState } from 'nuqs'
+import { useLayoutEffect, useRef, useState } from 'react'
 import { Link } from 'react-router'
 
 import { ViewablePersonProfileLine } from '../components/ViewablePersonProfileLine'
 import { useDebouncedSearchPersons, useGetPersons } from '../hooks/usePersons'
 
 const PAGE_SIZE = 50
+const ROW_HEIGHT = 53
 
 export const CommunityPage = () => {
   const [q, setQ] = useQueryState('q')
@@ -19,56 +22,97 @@ export const CommunityPage = () => {
 
   const isAutocompleteLoading = isDebouncing || usedSearchPersons.isFetching
 
+  const personsD = usedGetPersons.data?.pages.flat() ?? []
+  const persons = Array.from({ length: 20 }).flatMap(() => personsD)
+
+  const tableRef = useRef<HTMLTableElement>(null)
+  const [scrollMargin, setScrollMargin] = useState(0)
+  useLayoutEffect(() => {
+    setScrollMargin(tableRef.current?.offsetTop ?? 0)
+  }, [])
+
+  const virtualizer = useWindowVirtualizer({
+    count: persons.length,
+    estimateSize: () => ROW_HEIGHT,
+    overscan: 100,
+    scrollMargin,
+  })
+
+  const virtualItems = virtualizer.getVirtualItems()
+  const firstItem = virtualItems[0]
+  const lastItem = virtualItems[virtualItems.length - 1]
+  const paddingTop = firstItem ? firstItem.start - virtualizer.options.scrollMargin : 0
+  const paddingBottom = lastItem
+    ? virtualizer.getTotalSize() - (lastItem.end - virtualizer.options.scrollMargin)
+    : 0
+
   return (
-    <Stack mih="100dvh" p="xl" w="100%">
-      <Group w={'100%'}>
-        <Autocomplete
-          data={
-            isAutocompleteLoading
-              ? [{ value: '...', disabled: true }]
-              : (usedSearchPersons.data?.pages.flat().map(({ username }) => username) ?? [])
-          }
-          filter={({ options }) => options}
-          loading={usedGetPersons.isFetching}
-          placeholder="Search for a card..."
-          rightSection={<MagnifyingGlassIcon />}
-          style={{ flex: 1 }}
-          value={q ?? ''}
-          onChange={newValue => setQ(newValue ?? undefined)}
-        />
-      </Group>
-      <Table stickyHeader>
-        <Table.Thead>
-          <Table.Tr>
-            <Table.Th>Person</Table.Th>
-            <Table.Th>Followers</Table.Th>
-            <Table.Th>Cards Rated</Table.Th>
-            <Table.Th>Likes</Table.Th>
-            <Table.Th>Dislikes</Table.Th>
-            <Table.Th ta="right" />
-          </Table.Tr>
-        </Table.Thead>
-        <Table.Tbody>
-          {usedGetPersons.data?.pages.flat().map(person => (
-            <Table.Tr key={person.uuid}>
-              <Table.Td>
-                <Box w="fit-content">
-                  <ViewablePersonProfileLine loading={false} person={person} />
-                </Box>
-              </Table.Td>
-              <Table.Td>2</Table.Td>
-              <Table.Td>45</Table.Td>
-              <Table.Td>2</Table.Td>
-              <Table.Td>0</Table.Td>
-              <Table.Td>
-                <Button component={Link} to={{ pathname: `/community/${person.uuid}` }}>
-                  View
-                </Button>
-              </Table.Td>
+    <Box p="xl" w="100%">
+      <Stack gap="sm">
+        <Group w={'100%'}>
+          <Autocomplete
+            data={
+              isAutocompleteLoading
+                ? [{ value: '...', disabled: true }]
+                : (usedSearchPersons.data?.pages.flat().map(({ username }) => username) ?? [])
+            }
+            filter={({ options }) => options}
+            loading={usedGetPersons.isFetching}
+            placeholder="Search for a person..."
+            rightSection={<MagnifyingGlassIcon />}
+            style={{ flex: 1 }}
+            value={q ?? ''}
+            onChange={newValue => setQ(newValue ?? undefined)}
+          />
+        </Group>
+        <Table stickyHeader ref={tableRef}>
+          <Table.Thead>
+            <Table.Tr>
+              <Table.Th>Person</Table.Th>
+              <Table.Th>Followers</Table.Th>
+              <Table.Th>Cards Rated</Table.Th>
+              <Table.Th>Likes</Table.Th>
+              <Table.Th>Dislikes</Table.Th>
+              <Table.Th ta="right" />
             </Table.Tr>
-          ))}
-        </Table.Tbody>
-      </Table>
-    </Stack>
+          </Table.Thead>
+          <Table.Tbody>
+            {paddingTop > 0 && (
+              <Table.Tr>
+                <Table.Td colSpan={6} h={paddingTop} p={0} />
+              </Table.Tr>
+            )}
+            {virtualItems.map(virtualRow => {
+              const person = persons[virtualRow.index]
+              if (!person) return null
+
+              return (
+                <Table.Tr h={ROW_HEIGHT} key={virtualRow.key}>
+                  <Table.Td>
+                    <Box w="fit-content">
+                      <ViewablePersonProfileLine loading={false} person={person} />
+                    </Box>
+                  </Table.Td>
+                  <Table.Td>2</Table.Td>
+                  <Table.Td>45</Table.Td>
+                  <Table.Td>2</Table.Td>
+                  <Table.Td>0</Table.Td>
+                  <Table.Td ta="right">
+                    <Button component={Link} to={{ pathname: `/community/${person.uuid}` }}>
+                      View
+                    </Button>
+                  </Table.Td>
+                </Table.Tr>
+              )
+            })}
+            {paddingBottom > 0 && (
+              <Table.Tr>
+                <Table.Td colSpan={6} h={paddingBottom} p={0} />
+              </Table.Tr>
+            )}
+          </Table.Tbody>
+        </Table>
+      </Stack>
+    </Box>
   )
 }
