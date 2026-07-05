94d212cedf39c08a208db2d8b72f6547ecab0ed4
Ben <benjamin.colt.tarr@gmail.com>
2026-07-05 14:14:18 -0700

    fix error page

--- public/apps/the_peoples_edh_bracket/the-peoples-edh-bracket-client/src/pages/ErrorPage.tsx
+++ public/apps/the_peoples_edh_bracket/the-peoples-edh-bracket-client/src/pages/ErrorPage.tsx
@@ -1,24 +1,22 @@
-import { Center, Code, Stack, Text, Title } from '@mantine/core'
+import { Code, Stack, Text, Title } from '@mantine/core'
 import { useRouteError } from 'react-router'
 
 export const ErrorPage = () => {
   const error = useRouteError()
 
   return (
-    <Center flex={1} mih="100dvh">
-      <Stack>
-        <Title size="4rem">
-          An{' '}
-          <Text inherit c="red" component="span">
-            Error
-          </Text>{' '}
-          Occurred
-        </Title>
-        <Text c="dimmed" maw={540} size="xl">
-          You must have done something wrong.
-        </Text>
-        <Code block>{String(error)}</Code>
-      </Stack>
-    </Center>
+    <Stack p="lg">
+      <Title size="4rem">
+        An{' '}
+        <Text inherit c="red" component="span">
+          Error
+        </Text>{' '}
+        Occurred
+      </Title>
+      <Text c="dimmed" maw={540} size="xl">
+        You must have done something wrong.
+      </Text>
+      <Code block>{String(error)}</Code>
+    </Stack>
   )
 }
