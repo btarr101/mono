794ef9efb7970eb91bb2661431874e8606d66652
Ben <benjamin.colt.tarr@gmail.com>
2026-07-04 19:14:22 -0700

    default to 3001...

--- public/apps/the_peoples_edh_bracket/the_peoples_edh_bracket_server/src/config.rs
+++ public/apps/the_peoples_edh_bracket/the_peoples_edh_bracket_server/src/config.rs
@@ -4,7 +4,7 @@ use serde_inline_default::serde_inline_default;
 #[serde_inline_default]
 #[derive(Debug, Deserialize, Clone)]
 pub struct Config {
-    #[serde_inline_default("127.0.0.1:3000".to_string())]
+    #[serde_inline_default("127.0.0.1:3001".to_string())]
     pub bind_address: String,
     #[serde_inline_default(if cfg!(debug_assertions) { "dev".to_string() } else { "prod".to_string() })]
     pub stage: String,
