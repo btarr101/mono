8210a8dcab060ce671a4418d7d903bcf133a858c
Benjamin Tarr <91578843+btarr101@users.noreply.github.com>
2026-07-04 20:48:20 -0700

    Implement CRON job and buff memory (#49)
    * let's BUFF THIS
    
    * remove the other thing, but bump the instance

--- public/services/btarr.xyz/terraform/droplet.tf
+++ public/services/btarr.xyz/terraform/droplet.tf
@@ -2,7 +2,7 @@ resource "digitalocean_droplet" "droplet" {
   image  = "debian-13-x64"
   name   = "btarr.xyz"
   region = "sfo3"
-  size   = "s-1vcpu-1gb"
+  size   = "s-1vcpu-2gb"
 
   user_data = templatefile("${path.module}/cloud-init.yml.tpl", {
     ansible_ssh_public_key = data.digitalocean_ssh_key.ansible.public_key
